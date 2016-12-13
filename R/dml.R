c0ed79827d5b37434dd54605bdb194634729d37a
Adam McElhinney <adam.mcelhinney@uptake.com>
2016-12-12 18:37:08 -0600

    Added a dml.R file to store the Roxygen2 documentation for imports. Added functionality so that you can automatically call lfda, klfda and self from lfda without having to do lfda::lfda

---
+++ R/dml.R
@@ -0,0 +1,14 @@
+#' @name Import Statements
+#' @title Import Statements
+#' @rdname imports
+#' @description This file is for generating import statements in namespace for dependencies
+#' @export GdmDiag
+#' @export GdmFull
+#' @export dca
+#' @export rca
+#' @import MASS
+#' @importFrom lfda lfda
+#' @importFrom lfda klfda
+#' @importFrom lfda self
+#' @importFrom stats cov
+NULL
